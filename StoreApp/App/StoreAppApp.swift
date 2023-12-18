bb2c03cb4cf157cee0b7808a25e789d0c717f5c0
AnriBeridzee <aberidze02@gmail.com>
2023-12-18 17:23:24 +0400

    Initial Commit

---
+++ StoreApp/App/StoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppApp.swift
+//  StoreApp
+//
+//  Created by Macbook Air 13 on 18.12.23.
+//
+
+import SwiftUI
+
+@main
+struct StoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
